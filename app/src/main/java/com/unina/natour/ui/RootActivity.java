ee0254421f0e5f9a106f9792bc1c7f52d1b05547
archer-65 <mariogt2009@live.it>
2022-01-21 15:56:58 +0100

    Prime activity

---
+++ app/src/main/java/com/unina/natour/ui/RootActivity.java
@@ -0,0 +1,41 @@
+package com.unina.natour.ui;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.unina.natour.R;
+import com.unina.natour.databinding.ActivityRootBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class RootActivity extends AppCompatActivity {
+
+    private ActivityRootBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityRootBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_root);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        if(true) {
+            navController.navigate(R.id.navigation_login);
+        }
+    }
+
+}

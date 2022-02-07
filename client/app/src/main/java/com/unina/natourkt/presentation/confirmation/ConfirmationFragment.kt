bb6a0b25f88a30f62f339826cb55035668f79d83
Random_Leviathan <74977887+Random-Leviathan890@users.noreply.github.com>
2022-02-07 19:48:14 +0100

    Aggiunta schermata conferma codice

---
+++ client/app/src/main/java/com/unina/natourkt/presentation/confirmation/ConfirmationFragment.kt
@@ -0,0 +1,37 @@
+package com.unina.natourkt.presentation.confirmation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.unina.natourkt.R
+import com.unina.natourkt.databinding.FragmentConfirmationBinding
+
+
+class ConfirmationFragment : Fragment() {
+
+   private var _binding: FragmentConfirmationBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentConfirmationBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

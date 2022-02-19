5fff04d7b233a7ccc2a508edfebf9c0f2712e67c
Bianca-Chehade <78654296+ctrl-zzz@users.noreply.github.com>
2022-02-19 19:29:16 +0100

    Viewpager per tab schermata profilo

--- client/app/src/main/java/com/unina/natourkt/presentation/profile/ProfileFragment.kt
+++ client/app/src/main/java/com/unina/natourkt/presentation/profile/ProfileFragment.kt
@@ -1,14 +1,21 @@
 package com.unina.natourkt.presentation.profile
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import com.unina.natourkt.R
 import com.unina.natourkt.databinding.FragmentProfileBinding
 import com.unina.natourkt.presentation.base.fragment.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import dev.chrisbanes.insetter.applyInsetter
 
 @AndroidEntryPoint
 class ProfileFragment : BaseFragment() {
@@ -18,6 +25,10 @@ class ProfileFragment : BaseFragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private var portraitIcon: Drawable? = null
+    private var favoriteIcon: Drawable? = null
+    private var locationOnIcon: Drawable? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,15 +41,89 @@ class ProfileFragment : BaseFragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-       // val textView: TextView = binding.textNotifications
-       // notificationsViewModel.text.observe(viewLifecycleOwner) {
-       //     textView.text = it
-       // }
+        setupUi()
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tabLayout = binding.tabLayout
+        val viewPager2 = binding.viewPager
+        val demoCollectionAdapter = DemoCollectionAdapter(this)
+        viewPager2.adapter = demoCollectionAdapter
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.icon = portraitIcon
+                }
+                1 -> {
+                    tab.icon = favoriteIcon
+                }
+                2 -> {
+                    tab.icon = locationOnIcon
+                }
+            }
+        }.attach()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun setupUi() {
+        binding.topAppBar.applyInsetter {
+            type(statusBars = true) {
+                margin()
+            }
+        }
+
+        //recyclerView = binding.recyclerHome
+
+        //progressBar = binding.progressBar
+
+        portraitIcon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_portrait_24)
+        favoriteIcon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_favorite_24)
+        locationOnIcon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_location_on_24)
+    }
 }
+
+class DemoCollectionAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 100
+
+    override fun createFragment(position: Int): Fragment {
+        // Return a NEW fragment instance in createFragment(int)
+        val fragment = DemoObjectFragment()
+        fragment.arguments = Bundle().apply {
+            // Our object is just an integer :-P
+            putInt(ARG_OBJECT, position + 1)
+        }
+        return fragment
+    }
+}
+
+private const val ARG_OBJECT = "object"
+
+// Instances of this class are fragments representing a single
+// object in our collection.
+class DemoObjectFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_collection_object, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
+            val textView: TextView = view.findViewById(R.id.text1)
+            textView.text = getInt(ARG_OBJECT).toString()
+        }
+    }
+}
+

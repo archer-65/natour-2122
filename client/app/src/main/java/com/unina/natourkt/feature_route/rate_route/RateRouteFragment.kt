5384b7bde5d39c55a1b2113e86ead91cae33714b
archer-65 <mariogt2009@live.it>
2022-03-17 15:33:14 +0100

    Aggiunti Fragment segnalazione e valutazione

---
+++ client/app/src/main/java/com/unina/natourkt/feature_route/rate_route/RateRouteFragment.kt
@@ -0,0 +1,7 @@
+package com.unina.natourkt.feature_route.rate_route
+
+import androidx.fragment.app.Fragment
+
+class RateRouteFragment : Fragment() {
+
+}
---
+++ client/app/src/main/java/com/unina/natourkt/feature_route/report_route/ReportRouteFragment.kt
@@ -0,0 +1,7 @@
+package com.unina.natourkt.feature_route.report_route
+
+import androidx.fragment.app.Fragment
+
+class ReportRouteFragment : Fragment() {
+
+}
